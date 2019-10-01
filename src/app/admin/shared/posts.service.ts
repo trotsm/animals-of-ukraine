d0262cf1d2b3089708f0da35d7e482da666c3c05
Mykola Trots <trots86@gmail.com>
2019-10-01 15:34:46 +0300

    added posts to main page, filter and delete button for posts

--- src/app/admin/shared/posts.service.ts
+++ src/app/admin/shared/posts.service.ts
@@ -16,7 +16,24 @@ export class PostsService {
           ...post,
           id: response.name,
           date: new Date(post.date)
-        }
-      }))
+        };
+      }));
+  }
+
+  getAll(): Observable<Post[]> {
+    return this.http.get(`${environment.fbDBUrl}/posts.json`)
+      .pipe(map((response: {[key: string]: any}) => {
+        return Object
+          .keys(response)
+          .map(key => ({
+            ...response[key],
+            id: key,
+            date: new Date(response[key].date)
+          }));
+      }));
+  }
+
+  remove(id: string): Observable<void> {
+    return this.http.delete<void>(`${environment.fbDBUrl}/posts/${id}.json`);
   }
 }
